ab83522c4e5a731853cde0bf9968709c8cd37f4f
sanjra <sanjrabezzi@gmail.com>
2022-03-12 18:38:18 +1100

    removed files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <footer>
+        <p>Sanjra created this application using React.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Netlify link here.
         </a>
-      </header>
+      </footer>
     </div>
   );
 }
-
-export default App;
