111c88726747290f422ecc8e1dea5dfa53d2399a
sanjra <sanjrabezzi@gmail.com>
2022-03-14 17:45:45 +1100

    Info and Temp added

---
+++ src/WeatherTemperature.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("celsius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+  function showFahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+          °C |{" "}
+          <a href="/" onClick={showCelsius}>
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.fahrenheit)}</span>
+        <span className="unit">
+          <a href="/" onClick={showFahrenheit}>
+            °C
+          </a>{" "}
+          | °F
+        </span>
+      </div>
+    );
+  }
+}
