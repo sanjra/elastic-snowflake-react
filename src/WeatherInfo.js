2e91fc9a831114c5f2c48ffb856b0dac6e2e25bc
sanjra <sanjrabezzi@gmail.com>
2022-03-20 20:12:43 +1100

    Finishing touches

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -7,7 +7,7 @@ export default function WeatherInfo(props) {
   return (
     <div className="WeatherInfo">
       <h1>{props.data.city}</h1>
-      <h2>{props.data.country}</h2>
+      <h3>{props.data.country}</h3>
       <ul>
         <li>
           <FormattedDate date={props.data.date} />
@@ -19,9 +19,6 @@ export default function WeatherInfo(props) {
           <div className="clearfix">
             <div className="float-left">
               <WeatherIcon code={props.data.icon} size={52} />
-            </div>
-
-            <div className="float-left">
               <WeatherTemperature celsius={props.data.temperature} />
             </div>
           </div>
